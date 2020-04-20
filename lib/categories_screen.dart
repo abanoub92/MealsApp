ad05b95b02603654284aba7e1720ac80a0797c0d
abanoub ghebreal <abanoub.eskander15@gmail.com>
2020-04-20 22:38:26 +0200

    start build meals app in flutter

---
+++ lib/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './categorty_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DailyMeals'),),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES.map(
+          (data) => CategoryItem(title: data.title, color: data.color),
+        ).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,  //the width of every item in grid
+          childAspectRatio: 3 / 2,  //the height of item in grid
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
